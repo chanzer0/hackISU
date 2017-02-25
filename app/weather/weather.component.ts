c7ab685c579a531181d03a65dfc206e5fdf3d230
https://github.com/gould-ann/hackISU.git <lmreicks@iastate.edu>
2017-02-25 17:47:56 -0600

    clock

--- app/weather/weather.component.ts
+++ app/weather/weather.component.ts
@@ -12,11 +12,42 @@ import { WeatherService } from './shared/weather.service';
 export class WeatherComponent implements OnInit {
 	weather: Weather;
 
-	constructor(private weatherService: WeatherService) { }
+	constructor(private WeathService: WeatherService) { }
 
 	ngOnInit() {
-		this.weatherService.getWeatherByZip(51401, 'us').then((res) => {
-			this.weather = res;
-		});
-	}
+        this.ifNight()
+        //get zip and code from user
+        this.WeathService.getWeatherByZip(51401, 'us').then(res => {
+            this.weather = res;
+            this.weather.main.temp = Math.round(this.weather.main.temp * (9/5) - 459.67);
+            this.weather.main.temp_min = Math.round(this.weather.main.temp_min * (9/5) - 459.67);
+            this.weather.main.temp_max = Math.round(this.weather.main.temp_max * (9/5) - 459.67);
+            console.log(this.weather);
+        }, (error) => {
+            //FAILURE
+            console.log(error);
+        })
+    } 
+
+    getSettings() {
+        //get country into lower case, check length on zipcode
+        this.WeathService.getWeatherByZip(this.user[0].zipcode, 'us').then(res => {
+            this.weather = res;
+            this.weather.main.temp = Math.round(this.weather.main.temp * (9/5) - 459.67);
+            this.weather.main.temp_min = Math.round(this.weather.main.temp_min * (9/5) - 459.67);
+            this.weather.main.temp_max = Math.round(this.weather.main.temp_max * (9/5) - 459.67);
+            console.log(this.weather);
+        }, (error) => {
+            //FAILURE
+            console.log(error);
+        })
+    }
+
+    ifNight() {
+        if (this.hours > 20 || this.hours < 5) {
+            this.nighttime = "moon";
+        }
+        else {
+            this.nighttime = "sun";
+        }
 }
