3da8d0bd94e90b87e3a5f87a9bc1b8187fcdd855
lmreicks <lmreicks@iastate.edu>
2017-02-24 22:36:13 -0600

    weather service

---
+++ app/weather/shared/weather.model.ts
@@ -0,0 +1,14 @@
+export class Weather {
+    base:String;
+    clouds: Object;
+    cod: number;
+    coord: Object;
+    dt: number;
+    id: number;
+    main: Object;
+    name: String;
+    sys: Object;
+    visibility: number;
+    weather: String;
+    wind: Object;
+}
