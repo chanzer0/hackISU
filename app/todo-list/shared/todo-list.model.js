54f2305638a168ee558bddb1be5438224d5074b2
lmreicks <lmreicks@iastate.edu>
2017-02-25 14:26:44 -0600

    remove .js.map and .js files

--- app/todo-list/shared/todo-list.model.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var TodoList = (function () {
-    function TodoList() {
-    }
-    return TodoList;
-}());
-exports.TodoList = TodoList;
-//# sourceMappingURL=todo-list.model.js.map
